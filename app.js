cb30be551a126e668f6b253e8d8aae509f7d96bf
Julia Portseva <104313259+wrongju@users.noreply.github.com>
2023-01-24 22:59:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// document.querySelector('#click-search').oninput = function () {
-//   let val = this.value.trim();
-//   let items = document.querySelectorAll('.list li');
-//   if (val != '') {
-//     items.forEach((a) => {
-//       if (a.innerText.search(val) == -1) {
-//         a.classList.add('hide');
-//       } else {
-//         a.classList.remove('hide');
-//       }
-//     });
-//   } else {
-//     items.forEach((a) => {
-//       a.classList.remove('hide');
-//     });
-//   }
-// };
 
 let create = document.querySelector('.creater');
 let list = document.querySelector('.list');
@@ -29,13 +12,4 @@ create.addEventListener('click', () => {
   list.append(doci);
 
   number.textContent++
-
-//   function func() {
-//     let k = 0;
-//     function count() {
-//       return k++;
-//     }
-//   }
-
-//   number.textContent = func();
 });
